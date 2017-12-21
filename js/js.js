3aabf33a8134f5e5d352ae02e6369c8093a14708
zozulialalala <juliazv97@gmail.com>
2017-12-21 17:20:23 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,9 @@
+$('.sl').slick({
+   autoplay: true,
+   autoplaySpeed: 0,
+   speed: 4000,
+   cssEase: 'linear',
+   centerMode: true,
+   arrows: false,
+   variableWidth: true,
+  });
---
+++ js/my_slick.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+   $('.slick-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slick-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+    });
+});
